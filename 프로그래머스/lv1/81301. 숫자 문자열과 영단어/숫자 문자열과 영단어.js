489a23873c68f34b31bb9788f2e64f8389118ee4
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-07-26 16:19:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    let answer = s;
+    
+    const numberWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    
+    for(let i = 0; i < numberWords.length; i++){
+        if(answer.indexOf(numberWords[i]) === -1) {  
+            continue;
+        };
+        
+        answer = answer.replace(numberWords[i], i);
+        
+        if(answer.indexOf(numberWords[i]) !== -1) {
+            answer = answer.replace(numberWords[i], i);
+        }
+    }
+    
+    return +answer;
+    
+}
