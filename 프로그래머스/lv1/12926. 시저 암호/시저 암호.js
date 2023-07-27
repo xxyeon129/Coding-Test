ceac7573eb9395410993006695834aa341ffdf9a
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-07-27 20:05:24 +0900

    [level 1] Title: 시저 암호, Time: 11.09 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,24 +1,15 @@
-function replaceAlphabet(s, replaceChar, i) {
-    return s.substring(0, i) + replaceChar + s.substring(i + replaceChar.length)
-}
-
 function solution(s, n) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
     for(let i = 0; i < s.length; i++){
         if(s[i] === ' ') continue;
+        
+        const alphabet = upperAlphabet.includes(s[i]) ? upperAlphabet : lowerAlphabet;
         const index = alphabet.indexOf(s[i]);
+        const replaceChar = alphabet[(index + n) % alphabet.length];
         
-        if(index === -1) {
-            const index = alphabet.indexOf(s[i].toLowerCase());
-            
-            const replaceChar = alphabet[(index + n) % alphabet.length].toUpperCase();
-            s = replaceAlphabet(s, replaceChar, i)
-            continue;
-        }
-        
-        const replaceChar = alphabet[(index + n) % alphabet.length]
-        s = replaceAlphabet(s, replaceChar, i);
+        s = s.substring(0, i) + replaceChar + s.substring(i + replaceChar.length);
     }
     
     return s;
