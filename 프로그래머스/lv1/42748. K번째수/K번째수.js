9a5a0fc82dd8ec5161b60e9964d53e712363d8f3
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-07-29 16:38:11 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -2,7 +2,11 @@ function solution(array, commands) {
     const answer = [];
     
     for(let i = 0; i < commands.length; i++){
-        answer.push(array.slice(commands[i][0]-1, commands[i][1]).sort((a,b) => a-b)[commands[i][2]-1])
+        const command = commands[i];
+        const sliceArray = array.slice(command[0]-1, command[1]);
+        const sortArray = sliceArray.sort((a,b) => a-b);
+        
+        answer.push(sortArray[command[2]-1])
     }
     
     return answer; 
