c95835090a25fc254fe06fd855a1d8c68d4751c6
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-08-13 16:23:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.47 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -8,34 +8,32 @@ function solution(numbers, hand) {
         ['*', 0, '#']
     ]
     
-    const right = {position: '#', index: [3, 2]};
-    const left = {position: '*', index: [3, 0]};
+    const right = [3, 2];
+    const left = [3, 0];
     
     for(let i = 0; i < numbers.length; i++){
         const numberPosition = numbers[i];
         
         if(numberPosition === 1 || numberPosition === 4 || numberPosition === 7) {
             answer += 'L';
-            left.position = numberPosition;
 
             keypad.forEach((row, index) => {
                 for(let i = 0; i < row.length; i++){
                     if(row[i] === numberPosition){
-                        left.index.splice(0, 1, index);
-                        left.index.splice(1, 1, i);
+                        left.splice(0, 1, index);
+                        left.splice(1, 1, i);
                     }
                 }
             });
             
         }else if(numberPosition === 3 || numberPosition === 6 || numberPosition === 9){
             answer += 'R';
-            right.position = numberPosition;
             
             keypad.forEach((row, index) => {
                 for(let i = 0; i < row.length; i++){
                     if(row[i] === numberPosition){
-                        right.index.splice(0, 1, index);
-                        right.index.splice(1, 1, i);
+                        right.splice(0, 1, index);
+                        right.splice(1, 1, i);
                     }
                 }
             });
@@ -46,19 +44,17 @@ function solution(numbers, hand) {
                 keypad.forEach((row, index) => {
                     if(row[j] === numberPosition){
 
-                        const rightLength = Math.abs(index - right.index[0]) + Math.abs(j - right.index[1]);
-                        const leftLength = Math.abs(index - left.index[0]) + Math.abs(j - left.index[1]);
+                        const rightLength = Math.abs(index - right[0]) + Math.abs(j - right[1]);
+                        const leftLength = Math.abs(index - left[0]) + Math.abs(j - left[1]);
                    
                         if(leftLength < rightLength || (leftLength === rightLength && hand === 'left')){
                             answer += 'L';
-                            left.position = numberPosition;
-                            left.index.splice(0, 1, index);
-                            left.index.splice(1, 1, j);
+                            left.splice(0, 1, index);
+                            left.splice(1, 1, j);
                         }else {
                             answer += 'R';
-                            right.position = numberPosition;
-                            right.index.splice(0, 1, index);
-                            right.index.splice(1, 1, j);
+                            right.splice(0, 1, index);
+                            right.splice(1, 1, j);
                         }
                     }
                 });
