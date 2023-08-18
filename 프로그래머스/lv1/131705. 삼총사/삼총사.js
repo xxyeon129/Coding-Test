45e4fd96a9da52fe594d5ea0888962b260df25aa
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-08-18 20:59:00 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -4,7 +4,7 @@ function solution(number) {
     for(let i = 0; i < number.length; i++){
         for(let j = i + 1; j < number.length; j++){
             for(let k = j + 1; k < number.length; k++){
-                if(i!==j && j!==k && i!==k && (number[i] + number[j] + number[k]) === 0){
+                if(number[i] + number[j] + number[k] === 0){
                   answer++; 
                 }
             }
