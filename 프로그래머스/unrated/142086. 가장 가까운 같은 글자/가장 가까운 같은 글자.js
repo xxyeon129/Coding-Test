da2870b6ac86731553a09107a77abcbf1885b439
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-09-23 21:52:48 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 129.96 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,13 @@
 function solution(s) {
-    const result = [];
     const sArray = [...s];
     
-    sArray.forEach((el, index) => {
+    return sArray.map((el, index) => {
         if(sArray.indexOf(el) === index) {
-            result.push(-1);
+            return -1;
         }else{
             const findStrArr = sArray.slice(0, index).reverse();
             const distance = findStrArr.indexOf(el) + 1;
-            result.push(distance);
+            return distance;
         }
     });
-    
-    return result;
 }
