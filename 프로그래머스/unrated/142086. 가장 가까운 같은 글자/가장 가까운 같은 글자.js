5a104a8b64cf11af5b968cc40a011cae9a92cdc2
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-09-23 22:12:10 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.38 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,13 +1,6 @@
 function solution(s) {
-    const sArray = [...s];
-    
     return [...s].map((el, index) => {
-        if(s.indexOf(el) === index) {
-            return -1;
-        }else{
-            const findStr = s.slice(0, index);
-            const distance = index - findStr.lastIndexOf(el);
-            return distance;
-        }
+        const distance = s.slice(0, index).lastIndexOf(el);
+        return distance < 0 ? distance : index - distance;
     });
 }
