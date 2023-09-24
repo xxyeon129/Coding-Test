547fe014915536e0ab3c82da7ad76e254e081d93
yeon <102347529+xxyeon129@users.noreply.github.com>
2023-09-24 18:34:07 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,18 +1,13 @@
 function solution(cards1, cards2, goal) {
-    let cards1Array = [...cards1];
-    let cards2Array = [...cards2];
-    const compareArray = [];
-    
     for(let i = 0; i < goal.length; i++){
-        if(cards1Array[0] === goal[i]){
-            compareArray.push(cards1Array[0]);
-            cards1Array.shift();
-        }
-        if(cards2Array[0] === goal[i]){
-            compareArray.push(cards2Array[0]);
-            cards2Array.shift();
+        if(cards1[0] === goal[i]){
+            cards1.shift();
+        }else if(cards2[0] === goal[i]){
+            cards2.shift();
+        }else{
+            return 'No';
         }
     }
     
-    return compareArray.toString() === goal.toString() ? 'Yes' : 'No';
+    return 'Yes';
 }
