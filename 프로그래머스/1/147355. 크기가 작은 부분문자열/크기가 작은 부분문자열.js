7323a703f70734a767bb6bce8c8bc4bf9d30ba79
yeon <102347529+xxyeon129@users.noreply.github.com>
2024-02-02 22:57:06 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,15 +2,10 @@ function solution(t, p) {
     let result = 0;
     const tArray = [];
     
-    for(let i = 0; i < t.length; i++){
+    for(let i = 0; i <= t.length - p.length; i++){
         tArray.push(t.slice(i, i + p.length));
+        +tArray[i] <= +p && result++;
     }
-    
-    const filteredTArray = tArray.filter(el => el.length === p.length);
-    
-    filteredTArray.forEach(el => {
-        if(+el <= +p) result++;
-    })
     
     return result;
 }
