e7a9fcd55aeef7c13fc68f1bf6343395baa1d470
yeon <102347529+xxyeon129@users.noreply.github.com>
2024-02-15 23:35:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,11 @@
 function solution(food) {
     let result = '';
-    
+
     for(let i = 1; i < food.length; i++){
-        result += `${i}`.repeat(Math.floor(food[i] / 2));
+        if(food[i] !== 1){
+            result += `${i}`.repeat(Math.floor(food[i] / 2));
+        }
     }
-    
-    return result + '0' + [...result].reverse().join("");
+
+    return result + '0' + result.split("").reverse().join("");
 }
