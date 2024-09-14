045bcb93b95e3a8701cf941b371e0610e439fe0f
yeon <102347529+xxyeon129@users.noreply.github.com>
2024-09-15 01:16:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.78 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,30 @@
+const getMinScore = (arr) => Math.min.apply(null, arr);
+
+function solution(k, score) {
+    const answer = [];
+    
+    const honorArr = [...score.slice(0, k)];
+    let minScore = score[0];
+    
+    const answerLengthBeforeK = k > score.length ? score.length : k;
+    
+    for(let i = 0; i < answerLengthBeforeK; i++){
+        if(score[i] < minScore) {
+            answer.push(score[i]);
+            minScore = score[i];
+        }else{
+            answer.push(minScore);
+        }
+    }
+    
+    score.slice(k).forEach((e) => {
+        if(e > minScore) {
+            honorArr.splice(honorArr.indexOf(minScore), 1);
+            honorArr.push(e);
+        }
+        minScore = getMinScore(honorArr);
+        answer.push(minScore);
+    })    
+    
+    return answer;
+}
