cf627b6dfd614c408b6005a0bef35d7be11162fb
yeon <102347529+xxyeon129@users.noreply.github.com>
2024-09-29 17:10:16 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 27.10 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 10.24 ms
+메모리: 37.8 MB, 시간: 27.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 16:35:36
+2024년 09월 29일 17:10:14
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,10 +1,15 @@
 function solution(friends, gifts) {
+    // 주고 받은 선물 기록
     const giveTakeArr = friends.map(() => Array(friends.length).fill(0));
     
+    // 선물 지수 계산을 위한 주고 받은 선물 합산
     const giftCountObj = friends.reduce((acc, cur) => {
-        acc[cur] = [0, 0, 0];
+        acc[cur] = [0, 0];
         return acc;
     }, {});
+    
+    // 다음 달에 받을 선물 수
+    const takeGiftsCount = new Array(friends.length).fill(0);
     
     gifts.forEach(gift => {
         const friendArr = gift.split(" ");
@@ -20,7 +25,8 @@ function solution(friends, gifts) {
             if (giveFriendIdx !== takeFriendIdx){
                 // 선물을 주고받은 기록이 있을 경우: 더 많은 선물을 준 사람이 선물을 받음
                  if(giveTakeArr[giveFriendIdx][takeFriendIdx] > giveTakeArr[takeFriendIdx][giveFriendIdx]) {
-                     giftCountObj[`${friends[giveFriendIdx]}`][2] += 1;
+                     takeGiftsCount[giveFriendIdx] += 1;
+                     
                 // 선물을 주고받은 기록이 없거나, 주고받은 수가 같은 경우
                  }else if(giveTakeArr[giveFriendIdx][takeFriendIdx] === giveTakeArr[takeFriendIdx][giveFriendIdx]) {
                      // 선물 지수
@@ -29,17 +35,13 @@ function solution(friends, gifts) {
                      
                      // 선물 지수가 큰 사람이 선물을 받음
                      if(giftCount1 > giftCount2) {
-                         giftCountObj[`${friends[giveFriendIdx]}`][2] += 1;
+                         takeGiftsCount[giveFriendIdx] += 1;
                      }
                  }
             }
-           
         }
     }
     
-    // 모든 사람의 받을 선물 수
-    const takeGiftsCount = Object.values(giftCountObj).map(arr => arr[arr.length -1]);
     // 가장 선물을 많이 받을 사람의 선물 수
     return Math.max(...takeGiftsCount);
-    
 }
